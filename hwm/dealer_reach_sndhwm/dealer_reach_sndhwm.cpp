560f0a028a4c44810f78f9f79d4f8d16d76e2902
Wang Ke | 王可 <pthreadself@gmail.com>
2017-09-13 17:52:18 +0800

    dealer_reach_sndhwm

---
+++ hwm/dealer_reach_sndhwm/dealer_reach_sndhwm.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Wang Ke on 13/09/2017.
+//
+
+#include <zmq.hpp>
+#include <iostream>
+
+int main() {
+    const int sndhwm = 1000;
+    zmq::context_t ctx;
+    zmq::socket_t dealer(ctx, ZMQ_DEALER);
+    dealer.setsockopt(ZMQ_SNDHWM, sndhwm);
+
+
+    // for this program to exit normally since we'll not really
+    // start a router to received all these 'sent messages'
+    dealer.setsockopt(ZMQ_LINGER, 0);
+
+
+    dealer.connect("ipc://dealer_reach_sndhwm");
+
+    std::cout << "ZMQ_SNDHWM: " << sndhwm << std::endl;
+
+
+    // Test if we'll be blocked when sending the first through <ZMQ_SNDHWM>th message
+    bool ever_blocked = false;
+    for (int i = 0; i < sndhwm; i++) {
+        size_t ret = dealer.send("abc", 3, ZMQ_DONTWAIT);
+        if (ret == 0) {
+            std::cout << "blocked when sending message NO." << i+1 << std::endl;
+            ever_blocked = true;
+        }
+    }
+
+    if (!ever_blocked) {
+        std::cout << sndhwm << " messages sent in queue without blocking" << std::endl;
+    } else {
+        std::cout << "fatal error, unexpected ZeroMQ behaviour" << std::endl;
+        return -1;
+    }
+
+    // Test if we'll be blocked when sending the <ZMQ_SNDHWM + 1>th message
+    std::cout << "trying to send another message, it's expected to be blocked" << std::endl;
+    size_t ret = dealer.send("abc", 3, ZMQ_DONTWAIT);
+    if (0 == ret) {
+        std::cout << "it's blocked" << std::endl;
+    } else {
+        std::cout << "fatal error, unexpected ZeroMQ behaviour" << std::endl;
+        return -1;
+    }
+
+    // Given a ZMQ socket already in `mute status` due to having reached high water mark,
+    // test if this socket won't wake up zmq_poll of its ZMQ_POLLOUT event
+    zmq_pollitem_t item { (void *)&dealer, 0, ZMQ_POLLOUT, 0};
+    std::cout << "now, give it 5 seconds to allow the event loop be signaled by ZMQ_POLLOUT event..." << std::endl;
+    int count = zmq::poll(&item, 1, 5000);
+    if (count <= 0) {
+        std::cout << "5 seconds passed and received no ZMQ_POLLOUT event." << std::endl;
+    }
+
+    return 0;
+}
+
